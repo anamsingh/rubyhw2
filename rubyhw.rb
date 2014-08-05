f17a0a9e9f1c9745fae12ca7164c02069cc25eca
Ana Singh <anasingh@Anas-MacBook-Pro.local>
2014-08-05 17:18:22 -0400

    first version

---
+++ rubyhw.rb
@@ -0,0 +1,25 @@
+Create the following functions:
+
+Create a function that takes a string and adds the phrase “Only in America!” to the end of it
+
+
+put "only in America"
+end
+
+
+
+
+
+Create a function to find the maximum value in an array of numbers. For instance:
+[100,10,-1000]
+
+should return 100. Do not use Ruby’s built-in .max function.
+
+Create a function that takes two arguments - both of them arrays. Inside of the function, combine the arrays using the items from the first array as keys and the second array as values. For example, when these two arrays are supplied as arguments:
+
+[:toyota, :tesla]
+[“Prius”, “Model S”]
+
+they should return a hash like so:
+{toyota: “Prius”, tesla: “Model S”}
+
